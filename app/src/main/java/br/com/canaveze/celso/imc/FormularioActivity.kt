c35e0ea6464c145d436e55c6651a3acad050025f
logonrm <logonrm@fiap.com.br>
2018-02-23 09:36:56 -0300

    Initial commit

---
+++ app/src/main/java/br/com/canaveze/celso/imc/FormularioActivity.kt
@@ -0,0 +1,25 @@
+package br.com.canaveze.celso.imc
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_formulario.*
+
+class FormularioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario)
+
+        btncalcular.setOnClickListener{
+            val telaResultado = Intent(this, ResultadoActivity::class.java)
+            startActivity(telaResultado)
+            telaResultado.putExtra("PESO",inputpeso.editText?.text.toString())
+            telaResultado.putExtra("ALTURA",inputaltura.editText?.text.toString())
+        }
+        btnLimpar.setOnClickListener{
+            inputaltura.editText?.setText("")
+            inputpeso.editText?.setText("")
+        }
+    }
+}
---
+++ app/src/main/java/br/com/canaveze/celso/imc/ResultadoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.canaveze.celso.imc
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
